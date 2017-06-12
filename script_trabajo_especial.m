f97448ba2d4734f760a5525953c78756f39bb1ee
nahuel <nabubaci24@gmail.com>
2017-06-12 15:06:43 -0300

    ya quedo en funcionamiento todo

--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -1,17 +1,24 @@
 %script_trabajo_especial
+epsilon = 0.0001;
 
-epsilon = 0.001;%se inicializa el valor de epsiolon
+while(epsilon <= 0.1)
+tic
 %esto llama al montecarlo
-[probabilidad, todas_las_probabilidades] = calcular_probabilidad_sacar_cara(epsilon);
+[probabilidad, todas_las_probabilidades] = calcular_probabilidad(epsilon);
 % Mostramos por pantalla la probabilidad que nos dio
-fprintf('Probabilidad de acierto = %f: \n', probabilidad);
-% Mostramos por pantalla la probabilidad que nos dio
-fprintf('La probabilidad es de: %d\n', probabilidad);
-% Graficamos cómo fue evolucionando la probabilidad iteración a iteración
-figure, plot(todas_las_probabilidades);
-hold on
-plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
-xlabel('Tiradas');
-ylabel('Probabilidad');
-ylim([0 1]);
-legend('Variacion de la probabilidad', 'Probabilidad analitica');
+fprintf('Probabilidad = %f: \n', probabilidad);
+arreglo=length(todas_las_probabilidades);
+arreglo
+% Graficamos 
+ figure, plot(todas_las_probabilidades);
+ hold on
+ plot(ones(size(todas_las_probabilidades)) * 0.25, '--');
+ xlabel('Intentos');
+ ylabel('Probabilidad');
+ ylim([0 1]);
+ legend('Variacion de la probabilidad', 'Probabilidad analitica');
+ epsilon = (epsilon * 10)
+ tiempo = toc;
+ tiempo
+ end
+ 
